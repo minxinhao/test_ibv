8c3655269387f7aac3c69fa4a2ef050ae8ec7bed
minxinhao <550279741@qq.com>
2022-06-03 13:01:24 +0000

    Fail to add dpu-host test

--- test_dpu.sh
+++ test_dpu.sh
@@ -10,6 +10,8 @@ echo $msg>>$log
 for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
 do
     echo $msg_size
+   killall $IBV_CMD ssh
+   sshpass -p 'mxh'  ssh mxh@192.168.2.89 "killall $IBV_CMD ssh"
     $IBV_CMD -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  &
     sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD 192.168.1.89 -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null
 done
@@ -20,29 +22,36 @@ echo $msg>>$log
 for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
 do
     echo $msg_size
+   killall $IBV_CMD ssh
+   sshpass -p 'mxh'  ssh mxh@192.168.2.89 "killall $IBV_CMD ssh"
     $IBV_CMD  -n 10000000 -q 1 -x 1 -s $msg_size | tail -2 >>$log  &
     sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD 192.168.1.89 -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null 
 done
 
+# Notes:It fails to run sshpass ssh "ib_xxx" in dpu first and then to run ib_xxx ip in host
 #local-dpu send to host
-log=$IBV_CMD\_MSG_SIZE.txt
-echo "local-dpu send to host">$log
-echo $msg>>$log
-for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
-do
-    echo $msg_size
-    sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null &
-    $IBV_CMD 192.168.2.89 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  
-done
+# log=$IBV_CMD\_MSG_SIZE.txt
+# echo "local-dpu send to host">$log
+# echo $msg>>$log
+# for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
+# do
+#     killall $IBV_CMD
+#     sshpass -p 'mxh' ssh mxh@192.168.2.89 "killall $IBV_CMD"
+#     echo $msg_size
+#     $IBV_CMD 192.168.2.89 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  &
+#     sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null 
+# done
 
 
-#remote-dpu send to host
-log=$IBV_CMD\_MSG_SIZE.txt
-echo "remote-dpu send to host">$log
-echo $msg>>$log
-for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
-do
-    echo $msg_size
-    sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null &
-    $IBV_CMD 192.168.2.88 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  
-done
+# #remote-dpu send to host
+# log=$IBV_CMD\_MSG_SIZE.txt
+# echo "remote-dpu send to host">$log
+# echo $msg>>$log
+# for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
+# do
+#     killall $IBV_CMD
+#     sshpass -p 'mxh'  ssh mxh@192.168.2.88 "killall $IBV_CMD"
+#     echo $msg_size
+#     sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null &
+#     $IBV_CMD 192.168.2.88 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  
+# done
