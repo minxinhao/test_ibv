3df0158c01e5d8033d55b256b80a5459d829ed3a
minxinhao <550279741@qq.com>
2022-06-03 12:10:15 +0000

    Add -x 1 to set the same gid

--- test_dpu.sh
+++ test_dpu.sh
@@ -10,8 +10,8 @@ echo $msg>>$log
 for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
 do
     echo $msg_size
-    $IBV_CMD -n 10000000 -q 1 -s $msg_size | tail -2 >>$log  &
-    sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD 192.168.1.88 -n 10000000 -q 1 -s  $msg_size "> /dev/null
+    $IBV_CMD -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  &
+    sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD 192.168.1.89 -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null
 done
 
 #host send to remote-dpu
@@ -20,6 +20,29 @@ echo $msg>>$log
 for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
 do
     echo $msg_size
-    $IBV_CMD  -n 10000000 -q 1 -s $msg_size | tail -2 >>$log  &
-    sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD 192.168.1.88 -n 10000000 -q 1 -s  $msg_size "> /dev/null 
+    $IBV_CMD  -n 10000000 -q 1 -x 1 -s $msg_size | tail -2 >>$log  &
+    sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD 192.168.1.89 -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null 
+done
+
+#local-dpu send to host
+log=$IBV_CMD\_MSG_SIZE.txt
+echo "local-dpu send to host">$log
+echo $msg>>$log
+for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
+do
+    echo $msg_size
+    sshpass -p 'mxh'  ssh mxh@192.168.2.89 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null &
+    $IBV_CMD 192.168.2.89 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  
+done
+
+
+#remote-dpu send to host
+log=$IBV_CMD\_MSG_SIZE.txt
+echo "remote-dpu send to host">$log
+echo $msg>>$log
+for((msg_size=1;msg_size<=2<<14;msg_size=msg_size*2));
+do
+    echo $msg_size
+    sshpass -p 'mxh'  ssh mxh@192.168.2.88 "$IBV_CMD -n 10000000 -x 1 -q 1 -s  $msg_size "> /dev/null &
+    $IBV_CMD 192.168.2.88 -x 1 -n 10000000 -q 1  -s $msg_size | tail -2 >>$log  
 done
